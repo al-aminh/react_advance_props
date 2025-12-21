13d92ff8a186551ca59d8f1ec85a35a82302eed0
al-aminh <alaminhossain123245@gmail.com>
2025-12-21 12:49:05 +0600

    created the structure for the ref prop

--- src/components/RefProps.jsx
+++ src/components/RefProps.jsx
@@ -1,6 +1,53 @@
-import React from 'react'
+import { useRef, forwardRef } from "react"
+
+const CustomInput = forwardRef(({label, placeholder, className}, ref) => {
+  return (
+    <div></div>
+  )
+})
+
+CustomInput.displayName = "CustomInput"
 
 function RefProps() {
+
+  const inputRef = useRef(null);
+  const secondInputRef = useRef(null);
+
+  const focusInput = () => {
+    inputRef.current?.focus()
+  }
+
+  const getFirstInputValue = () => {
+    if(inputRef.current){
+      alert(`Input Value: ${inputRef.current.value}`)
+    }
+  }
+
+  const clearFirstValue = () => {
+    if(inputRef.current){
+      inputRef.current.value = ""
+      inputRef.current.focus()
+    }
+  }
+
+  const focusSecondInput = () => {
+    secondInputRef.current?.focus()
+  }
+
+  const getSecondInputValue = () => {
+    if(secondInputRef.current){
+      alert(`Second Input Value: ${secondInputRef.current.value}`)
+    }
+  }
+
+  const clearSecondInput = () => {
+    if(secondInputRef.current){
+      secondInputRef.current.value = ""
+      secondInputRef.current.focus()
+    }
+  }
+
+
   return (
     <div>RefProps</div>
   )
