bc66e216f316b8b12abb2dea533f0cb7cb433bbd
al-aminh <alaminhossain123245@gmail.com>
2025-12-21 14:01:05 +0600

    enhance RefProps component with custom input fields and action buttons

--- src/components/RefProps.jsx
+++ src/components/RefProps.jsx
@@ -2,7 +2,14 @@ import { useRef, forwardRef } from "react"
 
 const CustomInput = forwardRef(({label, placeholder, className}, ref) => {
   return (
-    <div></div>
+    <div >
+      <label className="text-xl font-bold">{label}</label>
+      <input
+        ref={ref}
+        placeholder={placeholder}
+        className={className}
+      />
+    </div>
   )
 })
 
@@ -49,7 +56,36 @@ function RefProps() {
 
 
   return (
-    <div>RefProps</div>
+    <div className="bg-gray-100 p-6 rounded-xl">
+      <h1 className="text-4xl font-bold mb-4">Ref Props</h1>
+      <p className="font-medium mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, nostrum.</p>
+      <div className="space-y-4">
+        <CustomInput
+          ref={inputRef}
+          label="First Input"
+          placeholder="Type Something"
+          className="w-full px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <CustomInput
+          ref={secondInputRef}
+          label="Second Input"
+          placeholder="Type Something else..."
+          className="w-full px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+      <div className="space-y-2 mt-4">
+        <div className="space-x-2">
+          <button onClick={focusInput} className="py-2 px-4 rounded-2xl bg-blue-600 text-white hover:bg-blue-800 transition-all">Focus First</button>
+          <button onClick={getFirstInputValue} className="py-2 px-4 rounded-2xl bg-green-600 text-white hover:bg-green-800 transition-all">Get First Value</button>
+          <button onClick={clearFirstValue} className="py-2 px-4 rounded-2xl bg-red-600 text-white hover:bg-red-800 transition-all">Clear First</button>
+        </div>
+        <div className="space-x-2">
+          <button onClick={focusSecondInput} className="py-2 px-4 rounded-2xl bg-blue-600 text-white hover:bg-blue-800 transition-all">Focus Second</button>
+          <button onClick={getSecondInputValue} className="py-2 px-4 rounded-2xl bg-green-600 text-white hover:bg-green-800 transition-all">Get Second Value</button>
+          <button onClick={clearSecondInput} className="py-2 px-4 rounded-2xl bg-red-600 text-white hover:bg-red-800 transition-all">Clear Second</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
