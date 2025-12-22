852a0fc42e9500d3481d97852fb94a9fa2a4e4db
al-aminh <alaminhossain123245@gmail.com>
2025-12-22 22:09:41 +0600

    add ThemeContext and ThemeProvider for theme management

--- src/components/ThemeToggler.jsx
+++ src/components/ThemeToggler.jsx
@@ -1,4 +1,23 @@
-import React from 'react'
+import { createContext, useState } from "react"
+
+const ThemeContext = createContext()
+
+export const ThemeProvider = ({children}) => {
+  const [theme, setTheme] = useState("light")
+  const ToggleTheme = () => {
+
+  }
+
+  const value = {
+    theme,
+    ToggleTheme,
+    isDard: theme === "dark"
+  }
+
+  return <ThemeContext.Provider value={value} >
+    {children}
+  </ThemeContext.Provider>
+}
 
 function ThemeToggler() {
   return (
